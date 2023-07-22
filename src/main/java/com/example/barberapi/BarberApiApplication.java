a658199dfe6d1d09892df224cee2104bf5315707
Hendel Fonseca <hendeletfonseca@gmail.com>
2023-07-22 18:07:04 -0300

    first commit

---
+++ src/main/java/com/example/barberapi/BarberApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.barberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberApiApplication.class, args);
+	}
+
+}
